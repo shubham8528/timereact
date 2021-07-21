4b2afd787ea13b1b263b33d2d7e7102779396640
shubham8528 <shubham.bisht@etelligens.in>
2021-07-21 21:31:48 +0530

    time container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
 
 function App() {
+  let time = new Date();
+  time = time.getHours();
+  let messages = "";
+  let times = new Date();
+  times = times.toLocaleTimeString();
+
+
+  let interval = setInterval(function() {
+    times=new Date();
+    times=times.toLocaleTimeString();
+  console.log(times) 
+  },1000)
+
+
+  if (time >= 1 && time < 12) {
+    messages = "good mrng shubham";
+  } else if (time >= 12 && time < 16) {
+    messages = "good Afternoon";
+  } else {
+    messages = "good Nyt";
+  }
+
+  // CSS////
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='box'>
+        <h1 className='mrngMsg'>heyy Shubham :{messages}</h1>
+        <p className='timing'></p>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,58 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App, * as shu from "./App";
+import "./Time.css"
 
 
 
-var time= new Date();
-time = time.getHours();
-var messages='';
-var times=new Date();
-times=times.toLocaleTimeString();
-
-// let interval = setInterval(() => {
-//   times=new Date();
-//   times=times.toLocaleTimeString();
-//   console.log(times);
-// },1000)
-
-if(time>=1 && time<12){
-   messages='good mrng shubham'
-}
-else if(time>=12 && time<16){
-  messages='good Afternoon'
-}
-else{
-  messages='good Nyt'
-}
-
-// CSS////
-var mrngMsg={
-  fontFamily: "'Caveat', cursive",
-  margin:'0px',
-  padding:'10px'
-}
-var box={
-  textAlign:'center',
-  margin:'200px auto',
-  background:'white',
-  width:'500px',
-  border:'5px solid rgb(3, 160, 255)',
-  boxShadow:'0px 0px 50px rgb(0, 229, 247)'
-}
-var timing={
-  margin:'10px auto',
-  width:'100px',
-  border:'2px solid rgb(0, 255, 153)',
-  background:'black',
-fontSize:'20px',
- padding:'10px',
- color:'rgb(0, 255, 153)'
- 
-}
 
 ReactDOM.render(
   <>
-  <div style={box}><h1 style={mrngMsg}>heyy Shubham :{messages}</h1>
-  <p style={timing}>{times}</p></div></>,document.getElementById('root')
+ <App />
+ </>
 
-)
+  ,document.getElementById('root')
+) 
