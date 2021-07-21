57f679fd41e11257a4a2f2fae3f2e89342276c88
shubham8528 <shubham.bisht@etelligens.in>
2021-07-21 17:18:01 +0530

    timeapp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+var time= new Date();
+time = time.getHours();
+var messages='';
+var times=new Date();
+times=times.toLocaleTimeString();
+
+// let interval = setInterval(() => {
+//   times=new Date();
+//   times=times.toLocaleTimeString();
+//   console.log(times);
+// },1000)
+
+if(time>=1 && time<12){
+   messages='good mrng shubham'
+}
+else if(time>=12 && time<16){
+  messages='good Afternoon'
+}
+else{
+  messages='good Nyt'
+}
+
+// CSS////
+var mrngMsg={
+  fontFamily: "'Caveat', cursive",
+  margin:'0px',
+  padding:'10px'
+}
+var box={
+  textAlign:'center',
+  margin:'200px auto',
+  background:'white',
+  width:'500px',
+  border:'5px solid rgb(3, 160, 255)',
+  boxShadow:'0px 0px 50px rgb(0, 229, 247)'
+}
+var timing={
+  margin:'10px auto',
+  width:'100px',
+  border:'2px solid rgb(0, 255, 153)',
+  background:'black',
+fontSize:'20px',
+ padding:'10px',
+ color:'rgb(0, 255, 153)'
+ 
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
+  <div style={box}><h1 style={mrngMsg}>heyy Shubham :{messages}</h1>
+  <p style={timing}>{times}</p></div></>,document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
